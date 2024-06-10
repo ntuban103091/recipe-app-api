336004544a61b4670e83e179967d310be01dfe0c
Nico Tuban <onicsbantu@gmail.com>
2024-06-11 03:45:24 +0800

    updates

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,6 @@
-"""Database modes."""
+"""Database modes"""
+
+from django.conf import settings
 
 from django.db import models
 from django.contrib.auth.models import (
@@ -42,3 +44,20 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    """Recipe Object"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
